e2978c0ac463adbfe3c97236eb32811759682b32
Nathan Black <nathanblack14686@gmail.com>
2022-05-24 14:02:51 -0500

    functions imported from student files

--- warmup_set1_test.py
+++ warmup_set1_test.py
@@ -1,2 +1,21 @@
 import unittest
+import math
 
+filename = "nblack_warmupset1.py"
+target = __import__(filename)
+
+circle_area = target.circle_area
+square_side = target.square_side
+rectangle_area = target.rectangle_area
+rectangle_perimeter = target.rectangle_perimeter
+distance = target.distance
+heavier_than_fluorine = target.heavier_than_fluorine
+double_evens_triple_odds = target.double_evens_triple_odds
+is_exothermic = target.is_exothermic
+is_spontaneous = target.is_spontaneous
+usable_innmr = target.usable_innmr
+
+
+
+class Test_WarmupSet1(unittest.TestCase):
+    pass
