737707bd202cb04ea3d836b5393e2d2ed5dfdfe4
Nathan Black <nathanblack14686@gmail.com>
2022-05-24 15:10:23 -0500

    nblack warmupset1 code complete

--- nblack_warmupset1.py
+++ nblack_warmupset1.py
@@ -5,62 +5,79 @@ def circle_area(radius):
     '''
     Returns the area of a circle, given its radius. Use math.pi to represent pi.
     '''
-    pass
+    return math.pi * radius ** 2
 
 def square_side(perimeter):
     '''
     Returns the side of a square, given its perimeter.
     '''
-    pass
+    return perimeter / 4
 
 def rectangle_area(length,width):
     '''
     Returns the area of a rectangle, given its length and width.
     '''
-    pass
+    return length * width
 
 def rectangle_perimeter(length,width):
     '''
     Returns the perimeter of a rectangle, given its length and width.
     '''
-    pass
+    return 2 * (length + width)
 
 def distance(x1, y1, x2, y2):
     '''
     Returns the distance between two points, given their Cartesian coordinates (x1, y1) and (x2, y2)
     '''
-    pass
+    return math.sqrt((y2-y1)**2 + (x2-x1)**2)
 
 def heavier_than_fluorine(mass_num):
     '''
     Returns True if mass_num is heavier than fluorine, and False otherwise.
     '''
-    pass
+    if mass_num > 18.998:
+        return True
+    else:
+        return False
 
 def double_evens_triple_odds(num):
     '''
     If num is even, return twice the value of num. If num is odd, return thrice the value of num.
     ''' 
-    pass
+    if num % 2 == 0:
+        return 2*num
+    else:
+        return 3*num
 
 def is_exothermic(enthalpy):
     '''
     Returns True if a reaction is exothermic, and False if not.
     '''
-    pass
+    if enthalpy < 0:
+        return True
+    else:
+        return False
 
 def is_spontaneous(enthalpy, entropy, temperature):
     '''
     Returns True if a reaction is spontaneous, and False if not. Assume that all three values are given in agreeing units
     (Kelvin, Joules)
     '''
-    pass
+    gibbs_free_energy = enthalpy - entropy * temperature
+    if gibbs_free_energy < 0:
+        return True
+    else:
+        return False
+
 
 def usable_innmr(atomic_number, atomic_mass):
     '''
     Returns True if an atomic isotope is usable in NMR (i.e., has a nuclear spin), and False if not.
     '''
-    pass
+    if atomic_number % 2 == 1 or atomic_mass % 2 == 1:
+        return True
+    else:
+        return False
 
 
 
