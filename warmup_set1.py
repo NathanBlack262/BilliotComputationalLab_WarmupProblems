9aaf96e0f58233b69bdb86129a0d002d0ccf4af9
Nathan Black <nathanblack14686@gmail.com>
2022-05-24 08:23:20 -0500

    warmup_set1.py completed

---
+++ warmup_set1.py
@@ -0,0 +1,68 @@
+import math
+
+
+def circle_area(radius):
+    '''
+    Returns the area of a circle, given its radius. Use math.pi to represent pi.
+    '''
+    pass
+
+def square_side(perimeter):
+    '''
+    Returns the side of a square, given its perimeter.
+    '''
+    pass
+
+def rectangle_area(length,width):
+    '''
+    Returns the area of a rectangle, given its length and width.
+    '''
+    pass
+
+def rectangle_perimeter(length,width):
+    '''
+    Returns the perimeter of a rectangle, given its length and width.
+    '''
+    pass
+
+def distance(x1, y1, x2, y2):
+    '''
+    Returns the distance between two points, given their Cartesian coordinates (x1, y1) and (x2, y2)
+    '''
+    pass
+
+def heavier_than_fluorine(mass_num):
+    '''
+    Returns True if mass_num is heavier than fluorine, and False otherwise.
+    '''
+    pass
+
+def double_evens_triple_odds(num):
+    '''
+    If num is even, return twice the value of num. If num is odd, return thrice the value of num.
+    ''' 
+    pass
+
+def is_exothermic(enthalpy):
+    '''
+    Returns True if a reaction is exothermic, and False if not.
+    '''
+    pass
+
+def is_spontaneous(enthalpy, entropy, temperature):
+    '''
+    Returns True if a reaction is spontaneous, and False if not. Assume that all three values are given in agreeing units
+    (Kelvin, Joules)
+    '''
+    pass
+
+def usable_innmr(atomic_number, atomic_mass):
+    '''
+    Returns True if an atomic isotope is usable in NMR (i.e., has a nuclear spin), and False if not.
+    '''
+    pass
+
+
+
+
+
